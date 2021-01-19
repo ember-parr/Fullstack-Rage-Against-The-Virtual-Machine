e734d0f660ec7e736ab83e91af81331b74dbc12b
corneliajohnson <corneliaj91@gmail.com>
2021-01-19 15:45:16 -0500

    added post handleControlledInputChange func

--- Tabloid-Fullstack/client/src/pages/PostForm.js
+++ Tabloid-Fullstack/client/src/pages/PostForm.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Button, Form, FormGroup, Label, Input, Col } from "reactstrap";
+import { useHistory } from "react-router-dom";
+import { UserProfileContext } from "../providers/UserProfileProvider";
 
 export const PostForm = () => {
+  const [categories, setCategories] = useState([]);
+  const [post, setPost] = useState();
+
+  const history = useHistory();
+  const user = JSON.parse(localStorage.getItem("userProfile"));
+
+  //get post
+  useEffect(() => {
+    if (post?.id) {
+      fetch(`/api/post/${post.id}`).then((data) => {
+        setPost(data.post);
+      });
+    }
+  }, []);
+
+  const handleControlledInputChange = (event) => {
+    const newPost = { ...post };
+    newPost[event.target.name] = event.target.value;
+    setPost(newPost);
+  };
+
+  const handleClickNewPost = (event) => {
+    console.log({
+      title: post.title,
+      content: post.content,
+      imageLocation: post.imageLocation,
+      pubishDateTime: post.publishDateTime,
+      IsApproved: false,
+      userProfileId: parseInt(user.id),
+      categoryId: post.categoryId,
+    });
+    // .then(() => history.push("/"));
+  };
+
   return (
     <div className="container border border-dark mt-5">
       <Form className="p-5">
@@ -11,15 +47,11 @@ export const PostForm = () => {
             Title
           </Label>
           <Col sm={10}>
-            <Input type="text" name="title" />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="title" sm={2}>
-            Sub-Title
-          </Label>
-          <Col sm={10}>
-            <Input type="text" name="subTitle" />
+            <Input
+              type="text"
+              name="title"
+              onChange={handleControlledInputChange}
+            />
           </Col>
         </FormGroup>
         <FormGroup row>
@@ -27,8 +59,12 @@ export const PostForm = () => {
             Category
           </Label>
           <Col sm={10}>
-            <Input type="select" name="category">
-              <option></option>
+            <Input
+              type="select"
+              name="category"
+              onChange={handleControlledInputChange}
+            >
+              <option value="0"></option>
             </Input>
           </Col>
         </FormGroup>
@@ -37,7 +73,11 @@ export const PostForm = () => {
             Publishing Date
           </Label>
           <Col sm={10}>
-            <Input type="date" name="publishingDate" />
+            <Input
+              type="date"
+              name="publishDateTime"
+              onChange={handleControlledInputChange}
+            />
           </Col>
         </FormGroup>
         <FormGroup row>
@@ -45,18 +85,28 @@ export const PostForm = () => {
             Header Image
           </Label>
           <Col sm={10}>
-            <Input type="file" name="file" id="exampleFile" />
+            <Input
+              type="file"
+              name="imageLocation"
+              onChange={handleControlledInputChange}
+            />
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label for="body" sm={2}>
+          <Label for="content" sm={2}>
             Body
           </Label>
           <Col sm={10}>
-            <Input type="textarea" name="body" />
+            <Input
+              type="textarea"
+              name="content"
+              onChange={handleControlledInputChange}
+            />
           </Col>
         </FormGroup>
-        <Button className="float-right">Submit</Button>
+        <Button className="float-right" onClick={handleClickNewPost}>
+          Submit
+        </Button>
       </Form>
     </div>
   );

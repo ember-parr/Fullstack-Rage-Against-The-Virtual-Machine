624875ce1ab6a14a43518fdb51e021cc7f0544f5
corneliajohnson <corneliaj91@gmail.com>
2021-01-19 14:47:16 -0500

    added form to create post

--- Tabloid-Fullstack/client/src/pages/PostForm.js
+++ Tabloid-Fullstack/client/src/pages/PostForm.js
@@ -1,3 +1,71 @@
+import React from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Col,
+} from "reactstrap";
+
 export const PostForm = () => {
-  return <h1>post form</h1>;
+  return (
+    <div className="container">
+      <Form>
+        <h2>Create A New Post</h2>
+        <FormGroup row>
+          <Label for="title" sm={2}>
+            Title
+          </Label>
+          <Col sm={10}>
+            <Input type="text" name="title" />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="title" sm={2}>
+            Sub-Title
+          </Label>
+          <Col sm={10}>
+            <Input type="text" name="subTitle" />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="exampleSelect" sm={2}>
+            Category
+          </Label>
+          <Col sm={10}>
+            <Input type="select" name="category">
+              <option></option>
+            </Input>
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="title" sm={2}>
+            Publishing Date
+          </Label>
+          <Col sm={10}>
+            <Input type="date" name="publishingDate" />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="exampleFile" sm={2}>
+            Header Image
+          </Label>
+          <Col sm={10}>
+            <Input type="file" name="file" id="exampleFile" />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="body" sm={2}>
+            Body
+          </Label>
+          <Col sm={10}>
+            <Input type="textarea" name="body" />
+          </Col>
+        </FormGroup>
+        <Button className="float-right">Submit</Button>
+      </Form>
+    </div>
+  );
 };

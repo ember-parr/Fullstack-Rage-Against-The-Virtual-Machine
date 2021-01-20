f52d964ee8d0938cb143ddcfc9b872ea887cf5c6
bloges56 <brady.drew.logan@gmail.com>
2021-01-20 13:16:02 -0600

    ready to test front end

--- Tabloid-Fullstack/client/src/components/Tag.js
+++ Tabloid-Fullstack/client/src/components/Tag.js
@@ -7,10 +7,43 @@ import {
   ModalBody,
   ModalFooter,
   ModalHeader,
+  Form,
+  Input,
+  InputGroup,
 } from "reactstrap";
 
 const Tag = ({ tag, deleteTag }) => {
+  const { getToken } = useContext(UserProfileContext);
+  const [isEditing, setIsEditing] = useState(false);
   const [pendingDelete, setPendingDelete] = useState(false);
+  const [tagEdits, setTagEdits] = useState("");
+
+  const showEditForm = () => {
+    setIsEditing(true);
+    setTagEdits(tag.name);
+  };
+
+  const hideEditForm = () => {
+    setIsEditing(false);
+    setTagEdits("");
+  };
+
+  const updateTag = () => {
+    setIsEditing(true);
+    tag.name = tagEdits;
+    getToken()
+      .then((token) =>
+        fetch("api/tag", {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(tag),
+        })
+      )
+      .then(hideEditForm);
+  };
 
   const handleDelete = () => {
     deleteTag(tag.id);
@@ -19,17 +52,38 @@ const Tag = ({ tag, deleteTag }) => {
 
   return (
     <div className="justify-content-between row">
-      <>
-        <div className="p-1">{tag.name}</div>
-        <ButtonGroup size="sm">
-          <Button
-            className="btn btn-danger"
-            onClick={(e) => setPendingDelete(true)}
-          >
-            Delete
-          </Button>
-        </ButtonGroup>
-      </>
+      {isEditing ? (
+        <Form className="w-100">
+          <InputGroup>
+            <Input
+              size="sm"
+              onChange={(e) => setTagEdits(e.target.value)}
+              value={tagEdits}
+            />
+            <ButtonGroup size="sm">
+              <Button onClick={updateTag}>Save</Button>
+              <Button outline color="danger" onClick={hideEditForm}>
+                Cancel
+              </Button>
+            </ButtonGroup>
+          </InputGroup>
+        </Form>
+      ) : (
+        <>
+          <div className="p-1">{tag.name}</div>
+          <ButtonGroup size="sm">
+            <Button className="btn btn-primary" onClick={showEditForm}>
+              Edit
+            </Button>
+            <Button
+              className="btn btn-danger"
+              onClick={(e) => setPendingDelete(true)}
+            >
+              Delete
+            </Button>
+          </ButtonGroup>
+        </>
+      )}
       {/* DELETE CONFIRM MODAL */}
       <Modal isOpen={pendingDelete}>
         <ModalHeader>Delete {tag.name}?</ModalHeader>

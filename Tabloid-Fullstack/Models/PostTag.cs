146b129bfff4e8f85d74f07a0ba39f67900936e3
bloges56 <brady.drew.logan@gmail.com>
2021-01-20 14:56:32 -0600

    created postTag model

---
+++ Tabloid-Fullstack/Models/PostTag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tabloid_Fullstack.Models
+{
+    public class PostTag
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public int TagId { get; set; }
+    }
+}

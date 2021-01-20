7c81d9985bccbadd657c978b8c49f8ff49a2fe27
bloges56 <brady.drew.logan@gmail.com>
2021-01-20 10:19:56 -0600

    an admin can now add a new tag

--- Tabloid-Fullstack/client/src/components/TagList.js
+++ Tabloid-Fullstack/client/src/components/TagList.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useState, useContext } from "react";
 import { UserProfileContext } from "../providers/UserProfileProvider"
+import {
+  ListGroup,
+  ListGroupItem,
+  Input,
+  InputGroup,
+  Button,
+} from "reactstrap";
 
 const TagList = () => {
     const [tags, setTags] = useState([]);
 
     const { getToken } = useContext(UserProfileContext);
+    const [newTag, setNewTag] = useState("");
 
-    useEffect(() => {
-         getToken().then((token) =>
+    const getTags = () => {
+      getToken().then((token) =>
             fetch("/api/tag", {
                 method: "GET",
                 headers: {
@@ -16,17 +24,61 @@ const TagList = () => {
             })
             .then(resp => resp.json())
             .then(setTags));
+    }
+
+    useEffect(() => {
+      getTags()
     }, []);
 
-  return (
-    <div>
-      {tags.map((tag) => (
-        <div className="m-4" key={tag.id}>
-          {tag.name}
+    const saveNewTag = () => {
+      const tagToAdd = { name: newTag };
+      getToken().then((token) =>
+        fetch("/api/tag", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(tagToAdd),
+        }).then(() => {
+          setNewTag("");
+          getTags();
+        })
+      );
+    };
+
+    return (
+      <div className="container mt-5">
+        <img
+          height="100"
+          src="/quill.png"
+          alt="Quill Logo"
+          className="bg-danger rounded-circle"
+        />
+        <h1>Tag Management</h1>
+        <div className="row justify-content-center">
+          <div className="col-xs-12 col-sm-8 col-md-6">
+            <ListGroup>
+              {tags.map((tag) => (
+                <ListGroupItem key={tag.id}>
+                  {tag.name}
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+            <div className="my-4">
+              <InputGroup>
+                <Input
+                  onChange={(e) => setNewTag(e.target.value)}
+                  value={newTag}
+                  placeholder="Add a new tag"
+                />
+                <Button onClick={saveNewTag}>Save</Button>
+              </InputGroup>
+            </div>
+          </div>
         </div>
-      ))}
-    </div>
-  );
+      </div>
+    );
 };
 
 export default TagList;

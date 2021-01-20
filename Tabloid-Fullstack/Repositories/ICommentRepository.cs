d973dc580745698f969a14dec83a935ceb8a0346
Ember Parr <ember.devops@gmail.com>
2021-01-20 15:46:01 -0600

    updated repositories and model for comments

--- Tabloid-Fullstack/Repositories/ICommentRepository.cs
+++ Tabloid-Fullstack/Repositories/ICommentRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿
+using System.Collections.Generic;
 using Tabloid_Fullstack.Models;
 
 namespace Tabloid_Fullstack.Repositories
@@ -6,6 +7,9 @@ namespace Tabloid_Fullstack.Repositories
     public interface ICommentRepository
     {
         void Add(Comment comment);
+        void Delete(int id);
+        Comment GetCommentById(int id);
         List<Comment> GetCommentsByPostId(int id);
+        void Update(Comment comment);
     }
 }

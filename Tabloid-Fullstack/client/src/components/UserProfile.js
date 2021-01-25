a696a0135d9da4ca5ac0a47558e7356984faf910
blaker814 <rab0196@yahoo.com>
2021-01-25 11:31:23 -0600

    Resized profile pics

--- Tabloid-Fullstack/client/src/components/UserProfile.js
+++ Tabloid-Fullstack/client/src/components/UserProfile.js
@@ -1,7 +1,11 @@
 export const UserProfile = ({ profile }) => {
     return (
         <div className="justify-content-between align-items-center row">
-            <img className="pl-2" src={profile.imageLocation} />
+            <img className="pl-2 sm"
+                src={profile.imageLocation}
+                style={{ width: '5em' }}
+                alt={`${profile.displayName} profile picture`}
+            />
             <div>{`${profile.firstName} ${profile.lastName}`}</div>
             <div>{profile.displayName}</div>
             <div className="pr-2">{profile.userTypeId === 1 ? "Admin" : "Author"}</div>

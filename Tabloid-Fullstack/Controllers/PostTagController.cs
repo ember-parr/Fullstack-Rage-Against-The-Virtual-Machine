c511977c7636f2213b3b560e985d43a070b3d616
bloges56 <brady.drew.logan@gmail.com>
2021-01-25 11:15:53 -0600

    can now add postTags through postman

---
+++ Tabloid-Fullstack/Controllers/PostTagController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid_Fullstack.Models;
+using Tabloid_Fullstack.Repositories;
+
+namespace Tabloid_Fullstack.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostTagController : ControllerBase
+    {
+        private IPostTagRepository _postTagRepo;
+        private IPostRepository _postRepo;
+        private ITagRepository _tagRepo;
+
+        public PostTagController(IPostTagRepository postTagRepo, IPostRepository postRepo, ITagRepository tagRepo)
+        {
+            _postTagRepo = postTagRepo;
+            _postRepo = postRepo;
+            _tagRepo = tagRepo;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var post = _postRepo.GetById(id);
+            if(post == null)
+            {
+                return BadRequest();
+            }
+            var postTags = _postTagRepo.GetByPostId(id);
+            return Ok(postTags);
+        }
+
+        [HttpPost]
+        public IActionResult Post(PostTag postTag)
+        {
+            var tag = _tagRepo.GetById(postTag.TagId);
+            var post = _postRepo.GetById(postTag.PostId);
+            if(tag == null || post == null)
+            {
+                return BadRequest();
+            }
+            _postTagRepo.Add(postTag);
+            return CreatedAtAction("Get", new { id = postTag.Id }, postTag);
+        }
+    }
+}
---
+++ Tabloid-Fullstack/Repositories/PostTagRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid_Fullstack.Data;
+using Tabloid_Fullstack.Models;
+
+namespace Tabloid_Fullstack.Repositories
+{
+    public class PostTagRepository : IPostTagRepository
+    {
+        private ApplicationDbContext _context;
+
+        public PostTagRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<PostTag> GetByPostId(int postId)
+        {
+            return _context.PostTag
+                .Include(pt => pt.Tag)
+                .Where(pt => pt.PostId == postId)
+                .ToList();
+        }
+
+        public void Add(PostTag postTag)
+        {
+            _context.Add(postTag);
+            _context.SaveChanges();
+        }
+    }
+}

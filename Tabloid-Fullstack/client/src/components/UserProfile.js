c1293afd27ab5d840a54fcdec773d6d9bec546c2
blaker814 <rab0196@yahoo.com>
2021-01-26 11:34:23 -0600

    Added views for profile manager

--- Tabloid-Fullstack/client/src/components/UserProfile.js
+++ Tabloid-Fullstack/client/src/components/UserProfile.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import { ListGroupItem, Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
-export const UserProfile = ({ profile, deactivateUser }) => {
-    const [pendingDelete, setPendingDelete] = useState(false);
+export const UserProfile = ({ profile, deactivateUser, activateUser }) => {
+    const [pending, setPending] = useState(false);
 
     const handleDeactivation = () => {
         deactivateUser(profile.id);
-        setPendingDelete(false);
+        setPending(false);
+    };
+
+    const handleActivation = () => {
+        activateUser(profile.id);
+        setPending(false);
     };
 
     return (
@@ -24,21 +29,23 @@ export const UserProfile = ({ profile, deactivateUser }) => {
                 </div>
             </ListGroupItem>
             <Button
-                color="danger"
+                color={profile.isActive ? "danger" : "primary"}
                 className="align-self-center ml-3"
-                onClick={(e) => setPendingDelete(true)}
+                onClick={(e) => setPending(true)}
             >
-                Deactivate
+                {profile.isActive ? "Deactivate" : "Activate"}
             </Button>
-            <Modal isOpen={pendingDelete}>
-                <ModalHeader>Deactivate {profile.displayName}?</ModalHeader>
+            <Modal isOpen={pending}>
+                <ModalHeader>{profile.isActive ? "Deactivate" : "Activate"} {profile.displayName}?</ModalHeader>
                 <ModalBody>
-                    Are you sure you want to deactivate this user?
+                    Are you sure you want to {profile.isActive ? "deactivate" : "activate"} this user?
                 </ModalBody>
                 <ModalFooter>
-                    <Button onClick={(e) => setPendingDelete(false)}>No, Cancel</Button>
-                    <Button className="btn btn-outline-danger" onClick={handleDeactivation}>
-                        Yes, Deactivate
+                    <Button onClick={(e) => setPending(false)}>No, Cancel</Button>
+                    <Button
+                        className={profile.isActive ? "btn btn-outline-danger" : "btn btn-outline-primary"}
+                        onClick={profile.isActive ? handleDeactivation : handleActivation}>
+                        {profile.isActive ? "Yes, Deactivate" : "Yes, Activate"}
                     </Button>
                 </ModalFooter>
             </Modal>
--- Tabloid-Fullstack/client/src/pages/ProfileManager.js
+++ Tabloid-Fullstack/client/src/pages/ProfileManager.js
@@ -1,19 +1,35 @@
 import { useState, useContext, useEffect } from "react";
 import { UserProfileContext } from "../providers/UserProfileProvider";
-import { ListGroup } from "reactstrap";
+import { ButtonGroup, ListGroup } from "reactstrap";
 import { UserProfile } from "../components/UserProfile";
 
 export const ProfileManager = () => {
     const { getToken } = useContext(UserProfileContext);
     const [userProfiles, setUserProfiles] = useState([]);
+    const [activePage, setActivePage] = useState(true);
 
     useEffect(() => {
-        getUserProfiles();
-    }, []);
+        activePage ? getActiveProfiles() : getInactiveProfiles();
+    }, [activePage]);
 
-    const getUserProfiles = () => {
+    const getActiveProfiles = () => {
         getToken().then((token) =>
             fetch("/api/userprofile", {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+                .then((res) => res.json())
+                .then((profiles) => {
+                    setUserProfiles(profiles);
+                })
+        );
+    };
+
+    const getInactiveProfiles = () => {
+        getToken().then((token) =>
+            fetch("/api/userprofile/inactive", {
                 method: "GET",
                 headers: {
                     Authorization: `Bearer ${token}`,
@@ -34,17 +50,36 @@ export const ProfileManager = () => {
                     Authorization: `Bearer ${token}`,
                 },
             })
-                .then(getUserProfiles)
+                .then(getActiveProfiles)
+        );
+    }
+
+    const activateUser = (id) => {
+        getToken().then((token) =>
+            fetch(`/api/userprofile/${id}`, {
+                method: "PUT",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+                .then(getInactiveProfiles)
         );
     }
 
     return (
         <div className="container my-5">
             <h1>User Profiles</h1>
+            <ButtonGroup vertical>
+                <Button active={activePage}>Active</Button>
+                <Button active={!activePage}>Deactivated</Button>
+            </ButtonGroup>
             <ListGroup>
                 {userProfiles.map(profile => (
                     <div className="row" key={profile.id}>
-                        <UserProfile profile={profile} deactivateUser={deactivateUser} />
+                        <UserProfile profile={profile}
+                            deactivateUser={deactivateUser}
+                            activateUser={activateUser}
+                        />
                     </div>
                 ))}
             </ListGroup>
